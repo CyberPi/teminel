0821ae8f3ea3ca338f85602f523b99f1661202f2
Julius Müller <juliusmuller@gmx.de>
2023-11-11 19:33:18 +0100

    Update stuff

--- load/repository.go
+++ load/repository.go
@@ -15,17 +15,22 @@ import (
 	"source.cyberpi.de/go/teminel/utils"
 )
 
-const loaderDir = "/tmp/teminel/loader"
-const mirrorDir = "/tmp/teminel/mirror"
+const bareDirectory = "/tmp/teminel/bare"
+const workingDirectory = "/tmp/teminel/working"
 
-func CloneBare(hostUrl string, path string, repositoryName string) error {
-	bareRepository := fmt.Sprintf("%v.git", repositoryName)
+func CloneBare(protocol string, host string, path string, repositoryName string) error {
+	bareRepository := repositoryName + ".git"
 	barePath := filepath.Join(path, bareRepository)
-	repositoryPath := filepath.Join(loaderDir, repositoryName)
+	workingPath := filepath.Join(workingDirectory, repositoryName)
+	// Check if working copy needs an update
+	if utils.VerifyPath(workingPath) {
+		updateTarballRepo(host, workingDirectory, repositoryName)
+	}
+
 	if !utils.VerifyPath(barePath) {
-		fmt.Println("Loading repository:", repositoryName, "Using git clone from:", hostUrl)
+		fmt.Println("Loading repository:", repositoryName, "Using git clone from:", host)
 		options := &git.CloneOptions{
-			URL:          hostUrl + bareRepository,
+			URL:          host + bareRepository,
 			SingleBranch: true,
 			Depth:        1,
 			Tags:         git.NoTags,
@@ -33,12 +38,7 @@ func CloneBare(hostUrl string, path string, repositoryName string) error {
 		_, err := git.PlainClone(barePath, true, options)
 		if err != nil {
 			fmt.Println("Try to get archive download due to error:", err)
-			url := fmt.Sprintf("https://github.com/%v/archive/refs/heads/main.tar.gz", repositoryName)
-			err = LoadTarball(url, repositoryPath)
-			if err != nil {
-				return err
-			}
-			repository, err := git.PlainInit(repositoryPath, false)
+			repository, err := git.PlainInit(workingPath, false)
 			if err != nil {
 				return err
 			}
@@ -50,15 +50,61 @@ func CloneBare(hostUrl string, path string, repositoryName string) error {
 			if err != nil {
 				return err
 			}
-			worktree.Commit("New commit", &git.CommitOptions{
-				Author: &object.Signature{
-					Name:  "Auto Maton",
-					Email: "teminel@cyberpi.de",
-					When:  time.Now(),
-				},
-			})
-			options.URL = repositoryPath
+
+			options.URL = workingPath
 			_, err = git.PlainClone(barePath, true, options)
+			return err
+		}
+	}
+	return nil
+}
+
+func updateTarballRepo(host string, path string, repositoryName string) error {
+	url := fmt.Sprintf("https://%v/%v/archive/refs/heads/main.tar.gz", host, repositoryName)
+	workingPath := filepath.Join(path, repositoryName)
+	err := LoadTarball(url, workingPath)
+	if err != nil {
+		return err
+	}
+	repository, err := openOrInit(workingPath)
+	if err != nil {
+		return err
+	}
+	err = commit(repository)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func openOrInit(path string) (*git.Repository, error) {
+	if utils.VerifyPath(path + "/.git") {
+		return git.PlainOpen(path)
+	} else {
+		return git.PlainInit(path, false)
+	}
+}
+
+func commit(repository *git.Repository) error {
+	worktree, err := repository.Worktree()
+	if err != nil {
+		return err
+	}
+	status, err := worktree.Status()
+	if err != nil {
+		return err
+	}
+	if !status.IsClean() {
+		options := &git.AddOptions{All: true}
+		worktree.AddWithOptions(options)
+		_, err := worktree.Commit("New commit", &git.CommitOptions{
+			Author: &object.Signature{
+				Name:  "Auto Maton",
+				Email: "teminel@cyberpi.de",
+				When:  time.Now(),
+			},
+		})
+		if err != nil {
 			return err
 		}
 	}
